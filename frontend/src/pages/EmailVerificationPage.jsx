837b5d23f9ea4c417baa31a917feb084af0d763f
samy <samuel.coroiu@student.upt.ro>
2025-03-31 23:02:03 +0300

    EmailVerification

---
+++ frontend/src/pages/EmailVerificationPage.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import toast from 'react-hot-toast';
+import { useAuthStore } from '../store/authStore';
+import { ArrowLeft } from 'lucide-react';
+
+const EmailVerificationPage = () => {
+  const [code, setCode] = useState(['', '', '', '', '', '']);
+  const inputRefs = useRef([]);
+  const navigate = useNavigate();
+
+  const { error, isLoading, verifyEmail } = useAuthStore();
+
+  const handleChange = (index, value) => {
+    const newCode = [...code];
+
+    // Handle pasted content
+    if (value.length > 1) {
+      const pastedCode = value.slice(0, 6).split('');
+      for (let i = 0; i < 6; i++) {
+        newCode[i] = pastedCode[i] || '';
+      }
+      setCode(newCode);
+
+      // Focus on the last non-empty input or the first empty one
+      const lastFilledIndex = newCode.findLastIndex((digit) => digit !== '');
+      const focusIndex = lastFilledIndex < 5 ? lastFilledIndex + 1 : 5;
+      inputRefs.current[focusIndex].focus();
+    } else {
+      newCode[index] = value;
+      setCode(newCode);
+
+      // Move focus to the next input field if value is entered
+      if (value && index < 5) {
+        inputRefs.current[index + 1].focus();
+      }
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (e.key === 'Backspace' && !code[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const verificationCode = code.join('');
+    try {
+      await verifyEmail(verificationCode);
+      navigate('/');
+      toast.success('Email verified successfully');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Auto submit when all fields are filled
+  useEffect(() => {
+    if (code.every((digit) => digit !== '')) {
+      handleSubmit(new Event('submit'));
+    }
+  }, [code]);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-white bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center text-gray-500">
+          Verify Your Email
+        </h2>
+        <p className="text-center text-gray-600 mb-6">
+          Enter the 6-digit code sent to your email address.
+        </p>
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="flex justify-between">
+            {code.map((digit, index) => (
+              <input
+                key={index}
+                ref={(el) => (inputRefs.current[index] = el)}
+                type="text"
+                maxLength="6"
+                value={digit}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleKeyDown(index, e)}
+                className="w-12 h-12 text-center text-2xl font-bold bg-gray-200 text-gray-700 border-2 border-gray-300 rounded-lg focus:border-blue-600 focus:outline-none"
+              />
+            ))}
+          </div>
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            type="submit"
+            disabled={isLoading || code.some((digit) => !digit)}
+            className="w-full bg-blue-600 text-white font-bold py-3 px-4 rounded-lg focus:outline-none transition duration-200 disabled:opacity-50"
+          >
+            {isLoading ? 'Verifying...' : 'Verify Email'}
+          </motion.button>
+        </form>
+      </div>
+
+      <div className="px-8 py-4 bg-gray-200 bg-opacity-50 flex justify-center">
+        <Link
+          to="/login"
+          className="text-sm text-blue-600 hover:underline flex items-center"
+        >
+          <ArrowLeft className="h-4 w-4 mr-2" /> Back to Login
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
+
+export default EmailVerificationPage;
